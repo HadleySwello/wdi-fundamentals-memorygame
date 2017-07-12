fe74429da619c53d0aa2533bec6d45b2dae68228
hadleyana <hadleysweck@gmail.com>
2017-07-12 11:33:55 -0400

    created game logic

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Up and running!")
+
+alert('Hello, friends.');
+
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+}
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
